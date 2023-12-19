4229563eedea99e51672e020e17ba58752dfb5e7
fatihylmz1 <fatihylmz27@icloud.com>
2023-12-19 16:10:47 +0300

    About Page responsive design completed

--- src/Layouts/Team.js
+++ src/Layouts/Team.js
@@ -7,15 +7,15 @@ export const Team = () => {
 
     return (
         <div className="bg-[#FAFAFA]">
-            <div className="flex flex-col flex-wrap justify-between pt-28 px-48">
+            <div className="flex flex-col flex-wrap justify-between pt-28 sm:px-48 px-12">
 
-                <div className="flex flex-col gap-4 flex-wrap items-center justify-center">
+                <div className="flex flex-col gap-4 flex-wrap items-center justify-center ">
                     <p className="text-[#252B42] text-[2.5rem] font-bold">Meet Our Team</p>
                     <p className="text-[#737373] text-center text-sm font-normal">Problems trying to resolve the conflict between<br />
                         the two major realms of Classical physics: Newtonian mechanics </p>
                 </div>
 
-                <div className="flex flex-row flex-wrap justify-between py-28">
+                <div className="flex sm:flex-row flex-wrap justify-between py-28 flex-col sm:gap-0 gap-8">
 
                     <div className="px-10 py-8 flex flex-col justify-between items-center border rounded-md bg-[#FFF] gap-2">
                         <img src={team1} className="bg-top w-32 h-32 rounded-full object-cover" />
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -13,31 +13,32 @@ export const AboutPage = () => {
     return (
         <div>
             <Header />
-            <div className="flex justify-center px-48  ">
-                <div className="w-full rounded-2xl flex flex-row justify-stretch items-center gap-12 z-1000 relative h-159 flex-wrap ">
-                    <div className="flex flex-col justify-between w-1/2 h-80 flex-wrap">
+            <div className="flex sm:flex-row justify-center sm:px-48 flex-col">
+                <div className="w-full rounded-2xl flex sm:flex-row sm:justify-stretch items-center gap-12 sm:z-1000 sm:relative sm:h-159 flex-wrap flex-col justify-between">
+                    <div className="flex flex-col justify-between sm:w-1/2 sm:h-80 flex-wrap sm:items-start items-center sm:gap-0 gap-6 sm:py-0 py-24">
                         <p className="text-header-blue text-base font-bold">ABOUT COMPANY</p>
-                        <p className="text-header-blue text-6xl font-bold">ABOUT US</p>
-                        <p className="text-gray-500 text-xl font-normal">We know how large objects will act,<br />
+                        <p className="text-header-blue sm:text-6xl text-[2.5rem] font-bold">ABOUT US</p>
+                        <p className="text-gray-500 text-xl font-normal sm:text-start text-center">We know how large objects will act,<br />
                             but things on a small scale.</p>
                         <button className="rounded bg-sky-500 text-white text-2xl font-bold w-60 h-16">Get Quote Now</button>
                     </div>
-                    <div className="w-1/2 flex justify-end z-2000 absolute ml-[40rem] h-full">
+                    <div className="sm:w-1/2 flex justify-end sm:z-2000 sm:absolute sm:ml-[40rem] h-full w-full sm:px-0 px-6">
                         <img src={noneImg} />
                     </div>
                 </div>
             </div>
 
-            <div className="flex flex-col gap-6 py-6 px-48">
-                <p className="text-[#E74040] text-sm font-normal">Problems trying</p>
-                <div className="flex flex-row justify-between">
-                    <p className="text-header-blue text-2xl font-bold">Met minim Mollie non desert<br /> Alamo est sit cliquey dolor do<br /> met sent.</p>
-                    <p className="text-[#737373] text-sm font-bold">Problems trying to resolve the conflict between the two major realms of<br /> Classical physics: Newtonian mechanics</p>
+            <div className="flex flex-col sm:gap-6 sm:py-6 sm:px-48 items-center sm:items-start gap-10 py-12">
+                <p className="text-[#E74040] text-sm font-normal sm:text-start text-center">Problems trying</p>
+
+                <div className="flex sm:flex-row justify-between w-full flex-col sm:items-start items-center sm:gap-0 gap-24">
+                    <p className="text-header-blue text-2xl font-bold ">Met minim Mollie non desert<br /> Alamo est sit cliquey dolor do<br /> met sent.</p>
+                    <p className="text-[#737373] text-sm font-bold sm:text-start text-center">Problems trying to resolve the conflict between the two major realms of<br /> Classical physics: Newtonian mechanics</p>
                 </div>
             </div>
 
 
-            <div className="flex flex-row justify-between px-48 py-20">
+            <div className="flex sm:flex-row justify-between sm:px-48 py-20 flex-col sm:gap-0 gap-8">
                 <div className="flex flex-col items-center">
                     <p className="text-[#252B42] text-[3.625rem] font-bold">15K</p>
                     <p className="text-[#737373] text-base font-bold">Happy Customers</p>
@@ -56,12 +57,12 @@ export const AboutPage = () => {
                 </div>
             </div>
 
-            <div className="px-48 py-28">
+            <div className="sm:px-48 py-28 px-12">
                 <img src={video} className="w-full" />
 
             </div>
             <Team />
-            <div className="flex flex-col justify-center gap-4 py-8 text-center bg-[#FAFAFA]">
+            <div className="flex flex-col justify-center gap-4 py-8 text-center bg-[#FAFAFA] sm:px-0 px-12">
                 <h3 className="text-header-blue text-[2.5rem] font-bold">Big Companies Are Here</h3>
                 <p className="text-link-color text-sm font-normal">Problems trying to resolve the conflict between<br />
                     the two major realms of Classical physics: Newtonian mechanics  </p>
@@ -69,16 +70,16 @@ export const AboutPage = () => {
             <Clients />
 
             <div className="w-full h-159 bg-[#2A7CC7] flex flex-row justify-between">
-                <div className="px-48 py-48">
-                    <div className="flex flex-col gap-6 flex-wrap">
-                        <p className="text-white text-base font-bold">WORK WITH US</p>
-                        <p className="text-white text-[2.5rem] font-bold">Now Let’s grow Yours</p>
-                        <p className="text-white text-xl font-normal">The gradual accumulation of information about atomic and<br /> small-scale behavior during the first quarter of the 20th </p>
+                <div className="sm:px-48 sm:py-48 px-16 py-28">
+                    <div className="flex flex-col gap-6 flex-wrap sm:items-start items-center">
+                        <p className="text-white text-base font-bold sm:text-start text-center">WORK WITH US</p>
+                        <p className="text-white text-[2.5rem] font-bold sm:text-start text-center">Now Let’s grow Yours</p>
+                        <p className="text-white text-xl font-normal sm:text-start text-center">The gradual accumulation of information about atomic and<br /> small-scale behavior during the first quarter of the 20th </p>
                         <button className="rounded border border-white text-white text-2xl font-bold w-60 h-16">Button</button>
                     </div>
                 </div>
 
-                <div>
+                <div className="hidden sm:flex">
                     <img src={unsplash} className="h-full object-cover" />
                 </div>
 
