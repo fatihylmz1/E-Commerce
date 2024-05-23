cc6b9ce6aaabff0c88a065a83bc7b7fc0cfa41e0
fatihylmz1 <fatihylmz27@icloud.com>
2024-05-23 14:34:19 +0300

    home page photos fixed

--- src/Layouts/ShopCards.js
+++ src/Layouts/ShopCards.js
@@ -2,16 +2,19 @@ import React from "react";
 import "../index.css";
 
 
+
 export const ShopCards = () => {
     return (
         <div className="sm:flex sm:flex-row  sm:pl-52 sm:pr-44 sm:py-20 sm:h-180 sm:justify-between flex-wrap flex flex-col pl-9 pr-9 sm:gap-0 gap-4">
 
             <div className="bg-shopping-card-1 sm:w-1/2 sm:h-auto bg-no-repeat  bg-scroll  flex flex-col justify-end items-start w-full h-[35rem] sm:bg-left-top bg-center">
+
                 <div className=" sm:w-96 h-56 bg-explore-blue opacity-85 flex flex-col justify-center pl-16 pr-36 w-full mb-2 sm:mb-6">
                     <h3 className="text-white text-2xl font-bold opacity-100">Top Product Of<br /> the Week</h3>
                     <button className="rounded-md bg-explore-blue border-2 opacity-100 border-white text-white text-sm font-bold w-44 h-14">EXPLORE ITEMS</button>
 
                 </div>
+
 
             </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'shopping-card-1': "url(https://s3-alpha-sig.figma.com/img/812e/3845/7d0cc7c0071c4e5eb3752a437fda3d0d?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Xk-dJTHByrnnUKbXcjVgnVVN7Ko4i9wIWdyKqmJHhGPLW-Eh-QqYbRCyCvUGz1aAAXRMH69GS-8-YjA7E66LbVhh3i0RjeD2eiv0hMPCp0uNFpAKB2MK0qA1XSakjAGGlxdbZOjooo2BFCIdhCItvbWWXHStDdlSnNfjIP301Aih3~MZ38eBBYHtxM3ylUa~luG3YZ7-5FwlEqbQEo9LxGgNuri8RufCkElETO~t1AsSWYqo9S1GLJIxDq8bFO~9t7FkQJUPoFdRTug6FD4ZWD9b~p4jFWtv9k~YdTJUebFdnai0HjgSrQDgChRVrFCCTCaTfcR080MDdxSPW7U4HQ__)",
-        'shopping-card-2': "url(https://s3-alpha-sig.figma.com/img/4587/86e9/d5e9865170a32e0687f0a17d8581b9c8?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eooXQ4JNAVX-BqznSi4m5qtCM4jeIkTa-KVS0HwKZ3f9o0bWeo94CBhHf8sy4mq~TMBxrtYwuyvUaz47kcVkKGtSzzB1GYLk91x87TnzmhcFeI4CUwgGr1xsqMOrNGHT9Antc9nOXZkKT2BIxpCbUo-H~cOe8mkCclCmkZRr5LblUrAVumtz0UKBIlLljjN1YPXxAFUIZOKDRdFjVdp-5fgKTvXj2gGe4NVuad0YiOeH3leNo8hhl8irseDJ~JwTXgAQG3J2tYcblnUaPQ3XMuRXRlyYtMeNXd7y97Cg1ZK2KI6vlcDYnC4a5qO0GAuEhOz9czesl28jA0nJRTLGCA__)",
-        'shopping-card-3': "url(https://s3-alpha-sig.figma.com/img/81b4/0a6b/ad298edf330ec5747ae93edc6118ce4a?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ehX8EuIfUbT0PO1RP50RHGsZGauX1tDZ01DMm8chURZbVqFi52FgfKAwu~jGWGFGv4o~1~TfEvpW11PuXCz0B1ECRjzX79HoqZqS~ItHrcXghwmBX2AY~z26tvXn5v3WwzQS24AnJ~Lue4vdh6Ub-gYXlDm1C8l8xi7qRVYX0S4b0uPr8REeOhzdY1tjf3Eul6HBOR8VsXffk2e2pMSZCe0DJoNqgh48Uoh5irmqoQ6NqYVmBvBkjkb9OnS96-~f6wQCcQIayTeds~YcSJl0wTLBMrFhukpcymuhxjgOLyjZHEC5Hc8UXXhqWzfMd3eHdU-THWElH1YpiE8LO910Gg__)",
+        'shopping-card-1': "url('/src/photos/mediabgcover1.png')",
+        'shopping-card-2': "url('/src/photos/mediabgcover2.png')",
+        'shopping-card-3': "url('/src/photos/mediabgcover3.png')",
         "slider-photo": "url(https://s3-alpha-sig.figma.com/img/96c8/6912/d491d421800e62998b9af7c838cc25d1?Expires=1704067200&Signature=qTsiqUygbJSqg6KND4~fRGYMMeGHB4ZBqkIObyFhvBkqnrt2AJOpJPrcGpP3~tfH2vc6bWghjNJmIrybMP97T4N4BYB61YNyvcoooMXsjBk3zDLwqWOe32~sWw5XJf5sb5klm2aPRgbpHnEhzYeaj2K9vIuU~AfJS1XQ-swdkpsFh27mg92w1gE4~w9qlTyOIrhdAGY6CY5iNC-NqQLP7zkSbCeOf88dLXF4rxQkIu3AGA4Sb5c5Qpp3MSQ8RtDdyaRkx7lxM~IIU~TJ~m61c~yGI9xhl~KzwZk-g1XFQqSxjTRS9HhvVLCvAuatLi7do0QR1fC8ppBpwWuBxEMwtw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4)",
 
 
